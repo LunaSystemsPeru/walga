9039eb79147eb41597bdbeb86130c24108c4908f
Andreaitah <xandreitahx@gmail.com>
2022-07-13 10:10:10 -0500

    corregir form-documentos-vehiculos

---
+++ intranet/contents/form-documentosvehiculos.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from mannatthemes.com/dastone/default/horizontal-index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 21 May 2021 20:34:16 GMT -->
+<head>
+    <meta charset="utf-8"/>
+    <title>Walga Inversiones | Transporte y Alquiler de Gruas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="Walga Inversiones | Transporte y Alquiler de Gruas" name="description"/>
+    <meta content="" name="author"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../assets/images/faviconwalga.ico">
+
+    <!-- App css -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="../assets/css/icons.min.css" rel="stylesheet" type="text/css"/>
+    <link href="../assets/css/metisMenu.min.css" rel="stylesheet" type="text/css"/>
+    <link href="../plugins/daterangepicker/daterangepicker.css" rel="stylesheet" type="text/css"/>
+    <link href="../assets/css/app.min.css" rel="stylesheet" type="text/css"/>
+
+</head>
+
+<body data-layout="horizontal" class="">
+
+<!-- Top Bar Start -->
+<?php require '../fixed/tob-bar.php' ?>
+<!-- Top Bar End -->
+<div class="page-wrapper">
+    <!-- Page Content-->
+    <div class="page-content">
+        <div class="container-fluid">
+            <!-- Page-Title -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="page-title-box">
+                        <div class="row">
+                            <div class="col">
+                                <h4 class="page-title">Registrar Documentos de Vehiculo</h4>
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="javascript:void(0);">Flota</a></li>
+                                    <li class="breadcrumb-item active">Documentos de Vehiculos</li>
+                                </ol>
+                            </div><!--end col-->
+
+                        </div><!--end row-->
+                    </div><!--end page-title-box-->
+                </div><!--end col-->
+            </div><!--end row-->
+            <!-- end page title end breadcrumb -->
+            <div class="row justify-content-center">
+                <div class="col-lg-10">
+                    <div class="card">
+                    </div><!--end card-header-->
+                    <div class="card-body">
+                        <form>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="exampleInputPassword1">Vehiculo</label>
+                                        <select class="form-control" aria-label="Default select example">
+                                            <option selected>Abrir para seleccionar</option>
+                                            <option value="1">One</option>
+                                            <option value="2">Two</option>
+                                            <option value="3">Three</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="documento">Documento</label>
+                                        <input type="text" class="form-control" id="documento" placeholder="Documento">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <label class="my-3">Fecha de Emision</label>
+                                        <input type="text" class="form-control" placeholder="Fecha de emision" id="Fechaemision">
+                                    </div><!--end col-->
+                                    <div class="col-md-6">
+                                        <label class="my-3">Fecha de Vencimiento</label>
+                                        <input type="text" class="form-control" placeholder="Fecha de Vencimiento" id="Fechavencimiento">
+                                    </div><!--end col-->
+                                </div><!--end row-->
+                                <div class="input-group mb-3">
+                                    <button class="btn btn-secondary" type="button" id="button-addon1"><i class="fas fa-search"></i></button>
+                                    <input type="text" class="form-control" placeholder="" aria-label="Buscar emisor" aria-describedby="button-addon1">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <input type="file" class="form-control" id="inputGroupFile02">
+                                    <label class="input-group-text" for="inputGroupFile02">File</label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="col-sm-10 ms-auto">
+                                        <label class="form-label" for="estado">Estado</label>
+                                        <div class="form-check form-switch form-switch-secondary">
+                                            <input class="form-check-input" type="checkbox" id="customSwitchSecondary"
+                                                   checked>
+                                            <label class="form-check-label" for="customSwitchSecondary">Activo</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="exampleInputPassword1">Choferes</label>
+                                        <select class="form-control" aria-label="Default select example">
+                                            <option selected>Abrir para seleccionar</option>
+                                            <option value="1">One</option>
+                                            <option value="2">Two</option>
+                                            <option value="3">Three</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                    </form>
+                </div><!--end card-body-->
+                <div class="card-footer">
+                    <div class="col-auto align-self-center">
+
+                        <a href="#" class="btn btn-sm btn-soft-primary">
+                            <i data-feather="plus" class="fas fa-plus mr-2"></i>
+                            Guardar Vehiculo
+                        </a>
+                    </div><!--end col-->
+                </div>
+            </div><!--end card-->
+        </div> <!-- end col -->
+    </div> <!-- end row -->
+
+</div><!--end row-->
+
+
+</div><!-- container -->
+</div>
+<!-- end page content -->
+</div>
+<!-- end page-wrapper -->
+<?php
+include('../fixed/footer.php');
+?>
+
+
+<!-- jQuery  -->
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/metismenu.min.js"></script>
+<script src="../assets/js/waves.js"></script>
+<script src="../assets/js/feather.min.js"></script>
+<script src="../assets/js/simplebar.min.js"></script>
+<script src="../assets/js/moment.js"></script>
+<script src="../plugins/daterangepicker/daterangepicker.js"></script>
+
+
+<!-- App js -->
+<script src="../assets/js/app.js"></script>
+
+</body>
+
+
+<!-- Mirrored from mannatthemes.com/dastone/default/horizontal-index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 21 May 2021 20:35:01 GMT -->
+</html>
