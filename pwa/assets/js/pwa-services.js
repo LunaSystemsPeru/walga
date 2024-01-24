e29578e226715f2566e0ff9409229c28a547f3d4
Luis <leog.1992@gmail.com>
2024-01-24 18:04:53 -0500

    walga pwa

---
+++ pwa/assets/js/pwa-services.js
@@ -0,0 +1,22 @@
+
+'use strict';
+/*
+if('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('service-worker.js').then(registration => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        }).catch(error => {
+            console.error('Service Worker registration failed:', error);
+        });
+    });
+}
+ */
+/*
+if(window.navigator && navigator.serviceWorker) {
+    navigator.serviceWorker.getRegistrations()
+        .then(function(registrations) {
+            for(let registration of registrations) {
+                registration.unregister();
+            }
+        });
+}*/
